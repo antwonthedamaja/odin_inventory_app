9c9c21f366428e32dde7e089ee188a9d7bcacbbc
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-10-09 19:14:32 -0400

    Add category schema and model

---
+++ models/category.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true }
+});
+
+categorySchema.virtual('url').get(function() {
+    return `/category/${this.name}`;
+});
+
+module.exports = mongoose.model("Category", categorySchema);
