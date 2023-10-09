e060b0b940d2aea61e01365143e99f5da5191ee8
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-10-09 19:11:31 -0400

    Create item schema and model

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var app = express();
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+main().catch(err => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    stock: { type: Number, min: 0, required: true },
+    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+    price: { type: Number, min: 0, required: true }
+});
+
+itemSchema.virtual('url').get(function() {
+    return `/inventory/${this._id}`;
+});
+
+module.exports = mongoose.model("Item", itemSchema);
